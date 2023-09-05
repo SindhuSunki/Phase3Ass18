256ce058c79987831c8284fd3a13000acddc92e6
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-09-05 16:57:50 +0530

    first commit

---
+++ Assignment18/Data/Assignment18DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment18.Models;
+
+namespace Assignment18.Data
+{
+    public class Assignment18DbContext : DbContext
+    {
+        public Assignment18DbContext (DbContextOptions<Assignment18DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment18.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Assignment18/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Assignment18.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Assignment18DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Assignment18DbContext") ?? throw new InvalidOperationException("Connection string 'Assignment18DbContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
